0104238b1cc6ff206daed591af8e4a4d1fc5ae59
Geofrey Killeta Mwakio <163838943+Mwakiogeofrey@users.noreply.github.com>
2025-07-23 18:45:46 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,
-           
-        console.log('MongoDB connected');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected'); // ✅ This is valid
     } catch (error) {
         console.error('MongoDB connection error:', error);
         process.exit(1);
